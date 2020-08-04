00923c246e9f4ca355c7146ab33a338c87243294
FrolovSv <66001545+FrolovSv@users.noreply.github.com>
2020-08-04 12:51:33 +0300

    .bat для сервисов (manual)

---
+++ Документация/Start all service.bat
@@ -0,0 +1,13 @@
+echo 
+
+net start WebTours
+echo Service - WebTours - started
+
+net start Grafana
+echo Service - Grafana - started
+
+net start Influxdb
+echo Service - Influxdb - started
+
+net start telegraf
+echo Service - telegraf - started
---
+++ Документация/Stop all service.bat
@@ -0,0 +1,13 @@
+echo 
+
+net stop WebTours
+echo Service - WebTours - Stop
+
+net stop Grafana
+echo Service - Grafana - Stop
+
+net stop Influxdb
+echo Service - Influxdb - Stop
+
+net stop telegraf
+echo Service - telegraf - Stop
