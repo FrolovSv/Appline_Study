1a42fd2652b9c1ae4be86437f0eebb5c75e81cd5
FrolovSv <66001545+FrolovSv@users.noreply.github.com>
2020-08-06 13:03:42 +0300

    Jmeter 1,2,3

--- Скрипты/03_SearcH_Itinerary_Delete/Action.c
+++ Скрипты/03_SearcH_Itinerary_Delete/Action.c
@@ -1,157 +1,118 @@
-Action()
-{
-	lr_start_transaction("03_Search_itinerary_delete");
+<!DOCTYPE html>
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+    <meta charset="utf-8" />
+<title>Клиенты</title>
+<!--MENU-->
+<meta name="description" content=" ">
+<meta name="keywords" content=" ">
+<meta property="og:image" content="logo-600x315.png">
+<link href="assets/favicon.ico" rel="shortcut icon" />
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<style type="text/css">
+		#upblock {
+			display:none;
+			width:100px;
+			position:fixed;
+			color:#ff862f;
+			cursor:pointer;
+		margin-left:1020px;
+			bottom:100px;
+			padding: 10px;
+			border: 1px solid #ff862f;
+			z-index:1001;
+		}
+		</style>
+  <link href="assets/application.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body class="scope-origin">
+    <div class="wrapper">
+      <div class="header"></div>
+	  
+
+  <!--Партнёры-->
+<div class="container">
+<h1 class="page-heading">Клиенты</h1>
+  <div class="jcarousel-brands">
+    <div class="jcarousel-arr jcarousel-rarr btn-rarr" data-target="#jcarousel_brands">
+      <i class="icon-rarr"></i>
+    </div>
+    <div class="jcarousel-arr jcarousel-larr btn-larr" data-target="#jcarousel_brands">
+      <i class="icon-larr"></i>
+    </div>
+    <div class="jcarousel" id="jcarousel_brands">
+      <ul>
+        <li><a href="clients.html" class="x-holder"><img src="img/3567_mtsbank-logo.png" alt="МТС Банк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/895_sber_logo_final_.png" alt="Сбербанк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/900__.jpg" alt="Центральный банк Российской Федерации" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/922_final.jpg" alt="Хоум Кредит энд Финанс Банк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/910_sanofi_logo_final_.jpg" alt="Sanofi" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/913_913_il.jpg" alt="Филип Морис Интернэшнл" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/1038_106978_900_.png" alt="Спутник" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/905_vtb24_logo_final_.jpg" alt="ВТБ 24" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/1000_logo_final_.jpg" alt="ДжиИ Мани Банк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/985_3474.jpg" alt="Альфа-Банк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/928_eldorado_.png" alt="Эльдорадо" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/934_logo.jpg" alt="Procter&Gamble" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/988_gazprom_logo_.jpg" alt="Газпромбанк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/2829_renesans.jpg" alt="Ренессанс Жизнь" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/916_megafon_logo_final_.jpg" alt="Мегафон" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/991_rayffayzenbank_logo_.jpg" alt="Райффайзенбанк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/kick.jpg" alt="БОЛЬ" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/994_final.jpg" alt="ТрансКредитБанк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/925_otp_final_.jpg" alt="ОТП Банк" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/919_mts_rus_logo_final_.jpg" alt="МТС" /></a></li><li><a href="clients.html" class="x-holder"><img src="img/1029_ibrae_logo_.gif" alt="ИБРАЭ" /></a></li>
+        
+      </ul>
+    </div>
+  </div>
+</div>
+  </div>
+
+
+<!--DOWN-->
+<div class="footer" itemscope itemtype="http://schema.org/Organization">
+  <div class="container">
+    <div class="footer-copyright">
+      <p>&copy; 2017</p>
+      <p><span itemprop="name">ООО «Аплана»</span></p>
+      <a class="footer-map x-ln" href="sitemap.html">Карта сайта</a>
+    </div>
+    <div class="footer-git">
+      <p class="footer-location"><span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">117485, Москва, ул. Профсоюзная, 84/32, корп. 1
+Бизнес-центр «Калужский» 
+</span>   </p>
+      <p class="footer-phone"><a href="tel:+74957107580" class="x-ln" itemprop="telephone">+7 (495) 710-75-80</a></p>
+      <!--<p class="footer-email"><a class="x-ln" href="mailto:info@aplana.com">info@aplana.com</a></p>-->
+    </div>
+    <div class="footer-media">
+      coped for training JMeter
+    </div>
+    
+  </div>
+</div>    
+    
+    
+
+<script type="text/javascript">
+
+	function httpGet(theUrl)
+	{
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+		xmlHttp.send( null );
+		return xmlHttp.responseText;
+	}
 	
-		lr_start_transaction("Load_start_Page");
-	
-			web_reg_save_param_ex(
-				"ParamName=userSession",
-				"LB=name=\"userSession\" value=\"",
-				"RB=\"/>",
-				"Ordinal=1",
-					SEARCH_FILTERS,
-				LAST);	
-		
-			web_reg_find("Text=Welcome to the Web Tours site", LAST);
-			
-			web_url("WebTours",
-				"URL=http://localhost:1080/WebTours/", 
-				"TargetFrame=", 
-				"Resource=0", 
-				"RecContentType=text/html", 
-				"Referer=", 
-				"Snapshot=t1.inf", 
-				"Mode=HTML", 
-				LAST);
-					
-		lr_end_transaction("Load_start_Page", LR_AUTO);
-		
-		lr_think_time(5);					
-	
-		lr_start_transaction("login_user");			
-			web_reg_find("Text=User password was correct", LAST);
-			
-			web_submit_data("login.pl",
-				"Action=http://localhost:1080/cgi-bin/login.pl",
-				"Method=POST",
-				"TargetFrame=body",
-				"RecContentType=text/html",
-				"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
-				"Snapshot=t2.inf",
-				"Mode=HTML",
-				ITEMDATA,
-				"Name=userSession", "Value={userSession}", ENDITEM,
-				"Name=username", "Value={User_Login}", ENDITEM,
-				"Name=password", "Value={User_Pass}", ENDITEM,
-				"Name=JSFormSubmit", "Value=off", ENDITEM,
-				"Name=login.x", "Value=60", ENDITEM,
-				"Name=login.y", "Value=9", ENDITEM,
-				LAST);			
-		lr_end_transaction("login_user",LR_AUTO);
-	
-		lr_think_time(5);			
-			
-		lr_start_transaction("goto_Flight");		
-			
-			web_reg_find("Text=Find Flight",
-			LAST);
-			
-			web_url("welcome.pl", 
-				"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
-				"TargetFrame=", 
-				"Resource=0", 
-				"RecContentType=text/html", 
-				"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
-				"Snapshot=t12.inf", 
-				"Mode=HTML", 
-				LAST);
-			
-		lr_end_transaction("goto_Flight",LR_AUTO);
-		
-		lr_think_time(5);
-	
-		lr_start_transaction("goto_Itinerary");	
+		function scrolltop() {
+			bodyelem = $("html,body");
+				bodyelem.animate({scrollTop: 0});
+		}
 
-			web_reg_find("Fail=Found",
-				"Text=No flights have been reserved",
-				"SaveCount=erroMess",
-				LAST);
-						
-			web_reg_save_param_ex(
-				"ParamName=flightId",
-				"LB=name=\"flightID\" value=\"",
-				"RB=-",
-				"Ordinal=ALL",
-				SEARCH_FILTERS,
-				LAST);	
-			
-			web_reg_find("Text={flightId_1}",
-	             "SaveCount=Count_before_Flight_1",
-					LAST);		
+		$(window).scroll(function() {
+			var cont_top = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
+			if(cont_top>=1500)
+				$('#upblock').fadeIn();
+			else 
+				$('#upblock').fadeOut();
+		});
+		
+		$('#upblock').click(function() {
+			scrolltop();
+		});	
+		
+	var div = document.getElementsByClassName('book')[0];
+	div.innerHTML = httpGet('http://localhost:8090/guestbook');
+		
+</script>
+<script src="assets/application.js"></script>
+<script src="header.js"></script>
 
-			web_url("Itinerary Button", 
-				"URL=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary", 
-				"TargetFrame=body", 
-				"Resource=0", 
-				"RecContentType=text/html", 
-				"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
-				"Snapshot=t3.inf", 
-				"Mode=HTML", 
-				LAST);			
-						
-		lr_end_transaction("goto_Itinerary", LR_AUTO);	
-					
-		lr_think_time(5);
-		
-		lr_start_transaction("Delete_first");
-		
-			if (atoi(lr_eval_string("{flightId_count}")) == 0){
-				lr_error_message("No flight to delete");		
-			}else {			
-					
-				web_reg_find("Fail=Found","Text={flightId_1}",
-	             	"SaveCount=Count_after_Flight_1",
-					LAST);	
+  </body>
 
-				web_submit_form("itinerary.pl",
-			        "Snapshot=t10.inf", 
-			        ITEMDATA, 
-			        "Name=1", "Value=on", ENDITEM,         
-			        "Name=removeFlights.x", "Value=73", ENDITEM, 
-			        "Name=removeFlights.y", "Value=15", ENDITEM, 
-			        LAST);			
-			}
-		lr_end_transaction("Delete_first", LR_AUTO);	
-		
-		
-		lr_think_time(5);
-		
-		lr_start_transaction("goto_home");
-			
-			web_reg_find("Text=Welcome, <b>{User_Login}</b>, to the Web Tours reservation pages.",
-				LAST);	
-			
-			web_url("welcome.pl", 
-				"URL=http://localhost:1080/cgi-bin/welcome.pl?page=menus", 
-				"TargetFrame=", 
-				"Resource=0", 
-				"RecContentType=text/html", 
-				"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
-				"Snapshot=t16.inf", 
-				"Mode=HTML", 
-				LAST);
-		
-		lr_end_transaction("goto_home",LR_AUTO);
-				
-		//SLA 15 секунд ожидание действий пользователя
-		lr_think_time(2);
-		//lr_think_time(48);
-
-		lr_think_time(7);
-
-		
-	lr_end_transaction("03_Search_itinerary_delete", LR_AUTO);
-
-
-	return 0;
-}
+</html>
